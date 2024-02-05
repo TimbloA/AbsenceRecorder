e91f4b73e92e947b48abbe9d0adb272925ce09c0
Adi Timblo <timblo.a@etoncollege.org.uk>
2024-02-05 20:42:00 +0000

    Added basic model for divisions and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Adi Timblo on 05/02/2024.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
